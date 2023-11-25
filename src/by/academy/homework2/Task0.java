d398d8fc23ee8a05227d09552698d071ec2c46ba
dwightjoker <jokermc@mail.ru>
2023-11-26 01:12:11 +0300

    task0 hw2 + lesson7

--- src/by/academy/homework2/Task0.java
+++ src/by/academy/homework2/Task0.java
@@ -2,14 +2,26 @@ package by.academy.homework2;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Task0 {
 
             // Complete the pairs function below.
             static int pairs (int k, int[] arr) {
+
                 int pairsCount = 0;
-                // write code here
+                Set<Integer> set = new HashSet<>();
+                for (int num : arr) {
+                    set.add(num);
+                }
+                for (int num : arr) {
+                    if (set.contains(num + k)) {
+                        pairsCount++;
+                    }
+                }
+
                 return pairsCount;
             }
 
@@ -26,11 +38,11 @@ public class Task0 {
                 int[] arr = new int[n];
 
                 String[] arrItems = scanner.nextLine().split(" ");
-                System.out.println(Arrays.toString(arr));
+                //System.out.println(Arrays.toString(arr));
 
                 for (int i = 0; i < n; i++) {
                     arr[i] = Integer.parseInt(arrItems[i]);
-                    System.out.println(arr[i]);
+                    //System.out.println(arr[i]);
                 }
 
                 int result = pairs(k, arr);
